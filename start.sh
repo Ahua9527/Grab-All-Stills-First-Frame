75ac2aa5b56cca1bddadce7f97543581d35dbdf5
Ahua <41805937+DITStack@users.noreply.github.com>
2021-11-16 10:15:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 filelist=$(find *.mov｜*.mmxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
 OLDIFS="$IFS"
 IFS=$"\n"
@@ -7,6 +6,6 @@ for filename in *.*
 #$(find -iname *.mov｜*.mmxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
 do
 	mkdir output
-    ffmpeg -i $filename -vframes 1 //Users/ahua/Desktop/Grab_All_Stills_First_Frame/output/${filename%.*}.png
+    ffmpeg -i $filename -vframes 1 //Users/XXXX/Desktop/Grab_All_Stills_First_Frame/output/${filename%.*}.png
 done
 IFS=$OLDIFS
