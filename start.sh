67e93c5bee2e01c0fae65ce7c50dc46acbe70ee9
Ahua <41805937+DITStack@users.noreply.github.com>
2021-11-16 10:12:51 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+
+
+filelist=$(find *.mov｜*.mmxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
+OLDIFS="$IFS"
+IFS=$"\n"
+for filename in *.*
+#$(find -iname *.mov｜*.mmxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
+do
+	mkdir output
+    ffmpeg -i $filename -vframes 1 //Users/ahua/Desktop/Grab_All_Stills_First_Frame/output/${filename%.*}.png
+done
+IFS=$OLDIFS
