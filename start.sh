bc6a159d69d2d0ca15b1601318a9481590c7ebdc
Ahua <41805937+DITStack@users.noreply.github.com>
2021-11-16 10:15:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ for filename in *.*
 #$(find -iname *.mov｜*.mmxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
 do
 	mkdir output
-    ffmpeg -i $filename -vframes 1 //Users/XXXX/Desktop/Grab_All_Stills_First_Frame/output/${filename%.*}.png
+    ffmpeg -i $filename -vframes 1 /Users/XXXX/Desktop/Grab_All_Stills_First_Frame/output/${filename%.*}.png
 done
 IFS=$OLDIFS
