adaa5c8315d32751c9cd1c7db087a51c0f4b360d
Ahua <41805937+DITStack@users.noreply.github.com>
2021-11-16 11:47:49 +0800

    Update video2jpg.sh

--- video2jpg.sh
+++ video2jpg.sh
@@ -1,8 +1,8 @@
-filelist=$(find *.mov｜*.mmxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
+filelist=$(find *.mov｜*.mxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
 OLDIFS="$IFS"
 IFS=$"\n"
 for filename in *.*
-#$(find -iname *.mov｜*.mmxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
+#$(find -iname *.mov｜*.mxf｜*.mp4|.flv|*.mkv|*.h264|*.hevc|*.m4v|*.ts|*.avi|*.rmvb|*.mpg)
 do
 	mkdir output
     ffmpeg -i $filename -vframes 1 $PWD/output/${filename%.*}.jpg
